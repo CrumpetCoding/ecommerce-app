aaea36188cd14dfdf53b3f81f8e741c54bff2522
CrumpetCoding <seannrowe@gmail.com>
2026-04-07 20:21:16 +0100

    Created nav bar with styles

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,12 @@
 import { createFileRoute } from '@tanstack/react-router'
+import Navbar from '@/components/Navbar'
 
 export const Route = createFileRoute('/')({ component: App })
 
 function App() {
     return (
         <main className="page-wrap px-4 pb-8 pt-14">
-            <h1>Index</h1>
+            <Navbar />
         </main>
     )
 }
