b7b1787421b3c49f3c956dd9ed648cda18bde845
CrumpetCoding <seannrowe@gmail.com>
2026-05-07 19:42:26 +0100

    Added pagination buttons

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -1,23 +1,38 @@
+import PaginationButton from "./PaginationButton"
+
 type PaginationProps = {
     currentPage: number,
     totalPages: number,
-    setCurrentPage: (page: number) => void;
+    setCurrentPage: (page: number) => void,
 }
 
 export default function Pagination({ currentPage, totalPages, setCurrentPage }: PaginationProps) {
+
     return (
         <div>
-            <button className="pagiantion-btn" disabled={currentPage <= 1} onClick={() => setCurrentPage(currentPage - 1)} >Back</button>
-            <span>{currentPage}</span>
-            {[1, 2, 3, 4, 5, totalPages].map(num => (
-                <button
-                    className={"pagiantion-btn " + (currentPage === num ? "active" : "")}
-                    onClick={() => setCurrentPage(num)}
-                >
-                    {num}
-                </button>
+            <PaginationButton
+                label="Back"
+                currentPage={currentPage}
+                pageNumber={Math.max(currentPage - 1, 1)}
+                totalPages={totalPages}
+                setCurrentPage={(page) => setCurrentPage(page)}
+            />
+            {[1, 2, 3, 4, 5].map(num => (
+                <PaginationButton
+                    key={num}
+                    currentPage={currentPage}
+                    pageNumber={num}
+                    totalPages={totalPages}
+                    setCurrentPage={(page) => setCurrentPage(page)}
+                />
             ))}
-            <button className="pagiantion-btn" disabled={currentPage >= totalPages} onClick={() => setCurrentPage(currentPage + 1)}>Next</button>
+            <PaginationButton
+                label="Next"
+                currentPage={currentPage}
+                pageNumber={Math.min(currentPage + 1, totalPages)}
+                totalPages={totalPages}
+                setCurrentPage={(page) => setCurrentPage(page)}
+            />
         </div>
     )
 }
--- src/components/PaginationButton.tsx
+++ src/components/PaginationButton.tsx
@@ -1,11 +1,23 @@
+import "./PaginationButton.css";
+
 type PaginationButtonProps = {
     currentPage: number,
     pageNumber: number,
     totalPages: number,
+    setCurrentPage: (page: number) => void,
+    label?: string,
 }
 
-export default function PaginationButton({ currentPage, pageNumber, totalPages }: PaginationButtonProps) {
+export default function PaginationButton({ currentPage, pageNumber, totalPages, setCurrentPage, label }: PaginationButtonProps) {
+    const hasLabel: boolean = !!label;
+
     return (
+        <button
+            className={"pagiantion-btn " + ((currentPage === pageNumber && !hasLabel) ? "active" : "")}
+            onClick={() => setCurrentPage(pageNumber)}
+            disabled={pageNumber > totalPages || currentPage === pageNumber}
+        >
+            {hasLabel ? label : pageNumber}
+        </button>
     )
 }
-
