2cd92d7c05bc9b1fa9c372ce30e43ff0fcb2712f
CrumpetCoding <seannrowe@gmail.com>
2026-06-04 19:37:29 +0100

    Set up product page layout

--- src/components/ProductPage.tsx
+++ src/components/ProductPage.tsx
@@ -1,13 +1,20 @@
 import type { Product } from "@/api";
+import './ProductPage.css'
 import { formatCurrencyNumber, } from "@/utilities/numberFormat";
 
 
 export default function ProductPage({ product }: { product: Product }) {
-    return (<div>
-        <h1>{product.title}</h1>
-        <p>{formatCurrencyNumber(product.price)}</p>
+
+    return (<div className="product-container">
+        {/* <p>{formatCurrencyNumber(product.price)}</p>
         <p>⭐: {product.rating.rate}, Count: {(product.rating.count)}</p>
-        <p>{product.category}</p>
-        <img src={product.image} alt={`Image of a ${product.title}`} />
+        <p>{product.category}</p> */}
+
+        <div className="image-section">
+            <img className="product-image" src={product.image} alt={`Image of a ${product.title}`} />
+        </div>
+        <div className="product-details">
+            <h1>{product.title}</h1>
+        </div>
     </div>)
 }
