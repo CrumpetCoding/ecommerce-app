a6f1a1e363b8bed6c9b73ff6afa0007c7ace408c
CrumpetCoding <seannrowe@gmail.com>
2026-06-17 19:11:42 +0100

    Added star rating to product page

--- src/components/ProductPage.tsx
+++ src/components/ProductPage.tsx
@@ -1,9 +1,24 @@
 import type { Product } from "@/api";
 import './ProductPage.css'
 import { formatCurrencyNumber, } from "@/utilities/numberFormat";
+import { useMemo } from "react";
 
 
 export default function ProductPage({ product }: { product: Product }) {
+
+    const starCount = useMemo(() => {
+        let rating = product.rating.rate;
+        const hasHalfStar = rating % 1 !== 0;
+
+        if (hasHalfStar) {
+            rating = rating - 0.5;
+        }
+
+        return {
+            full: rating,
+            hasHalfStar
+        }
+    }, [product.rating.rate])
 
     return (<div className="product-container">
         {/* <p>{formatCurrencyNumber(product.price)}</p>
@@ -15,8 +30,8 @@ export default function ProductPage({ product }: { product: Product }) {
         </div>
         <div className="product-details">
             <h1>{product.title}</h1>
-            <IMdiStar />
-            <IMdiStarHalf />
+            {starCount.full > 0 && [...Array(starCount.full)].map((_, i) => <IMdiStar key={i} />)}
+            {starCount.hasHalfStar && <IMdiStarHalf />}
         </div>
     </div>)
 }
